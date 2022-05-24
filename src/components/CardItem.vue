6793d91eb1a2cedbf8b24b6e8796719aff9794ac
Silvia-Du <silviadu@hotmail.it>
2022-05-24 18:58:18 +0200

    Reso più moderno l'hover delle card

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,40 +1,35 @@
 <template>
 
-  <div class="_card d-flex flex-column">
+ 
 
-      
 
+    <div class="_card d-flex mx-5">
+
+      <div class="img-box">
         <img class="img-fluid" :src="`${imgUrl}${card.poster_path}`" :alt="card.title">
         <img v-if="card.poster_path == null" :src="require('../assets/img/img-alt.png')" alt="">
-        
-        <!-- layover -->
-        <div class="layover text-center d-flex flex-column justify-content-center align-items-center">
-          <p class="title">Title: {{card.title || card.name}}</p>
+      </div>
+
+      <div class="text-box pt-5">
+        <div class="text">
+          <p class="title text-uppercase">{{card.title || card.name}}</p>
           <p>Original title:{{card.original_title || card.original_name}}</p>
 
-          <!-- FLAG -->
           <span v-if="card.original_language == 'en'">lingua originale :<flag iso="gb"/></span>
           <span v-else>lingua originale :<flag :iso="card.original_language"/></span>
 
-          <!-- STAR -->
-          <div class="d-flex my-3">
-            <span>Vote:</span>
+          <div class="d-flex my-3 justify-content-center align-items-center">
+            <span class="me-2">Vote:</span>
             <i v-for="(string, index) in fullStar" :key="index" :class="`fa-${string} fa-star`"></i>
           </div>
 
-          <!-- BUTTON -->
           <button 
           @click="shwInfo()"
           type="button" class="btn btn-danger">More Info</button>
-          <span v-show="genresCard.length > 0 ">Genere:</span>
-          <span v-for="(genre, index) in genresCard" :key="index">{{genre.name,}}</span>
-          <span v-for="(actor, index) in cardCast" :key="index">{{actor.original_name,}}</span>
-        <!-- <MoreInfoComp v-show="clickedCard === card.id" :data="card.id"/> -->
 
-
-        </div>
-
-  </div>
+        </div> 
+      </div>
+    </div>
 
 </template>
 
@@ -62,7 +57,7 @@ export default {
 
             genresCard: [],
             cardCast:[],
-            getCredits:[' ', '/credits']
+            getCredits:[' ', '/credits'],
 
         };
     },
@@ -77,21 +72,18 @@ export default {
           .then(response =>{
 
             if(item === ' '){
-              console.log(response.data.genres, 'generi');
               this.genresCard =  response.data.genres;
             }
 
             if(item === '/credits'){
               this.cardCast =  response.data.cast;
               this.cardCast.length = 4;
-              console.log(this.cardCast, 'eccolo');
-            }
-            
+            } 
           })
         })
-
-      }
+      },
     },
+
     computed: {
         fullStar() {
             let star = [];
@@ -100,6 +92,7 @@ export default {
             for (let i = 0; i < fullStar; i++) {
                 star.push("solid");
             }
+
             for (let i = 0; i < rest; i++) {
                 star.push("regular");
             }
@@ -112,67 +105,81 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../assets/style/vars.scss';
+
 
   ._card{
     flex-shrink: 0;
-    border-radius: 0.40rem;
-    position: relative;
-    width: 250px;
     height: 355px;
     margin: 30px 10px;
+    border-radius: 10px;
     overflow: hidden;
-    background-color: rgb(218, 212, 212);
-    box-shadow: 10px 10px 15px black;
-    transition: all 1s;
-    background-image: require('../assets/img/alt.png');
+    transition: all 2s;
+    background-color: $bgColor;
+
+    
+    
     &:hover{
-      transform: scale(1.2);
-      z-index: 998;
+      transform: scale(1.1);
+      border-radius: 10px;
     }
-    &:Hover .layover{
+    &:hover .text-box{
+      width: 250px;
+      padding: 10px;
+      border-radius: 0 10px 10px 0;
       opacity: 100%;
     }
 
-    img{
-      transition: all 1s;
+    &:hover .img-box{
+      border-radius: 10px 0px 0px 10px;
+      img{
+        border-radius: 10px 0px 0px 10px;
+      }
     }
 
-    .layover{
-      width: 100%;
+
+      &:hover .text-box p{
+        opacity: 100%;
+      }
+      
+      
+      .text-box{
+        width: 0px;
+        height: 100%;
+        transition: all .5s;
+        overflow: hidden;
+        opacity: 0%;
+        background-color: lighten($bgColor, 5%);
+        p{
+          opacity: 0%;
+          color: white;
+        }
+
+        i{
+          color: rgb(255, 187, 0);
+        }
+
+        .btn{
+          background-color: $redColor;
+          font-size: 0.9rem;
+        }
+      }
+
+    .img-box{
+      width: 250px;
       height: 100%;
-      padding: 15px;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      font-size: 0.9rem;
-      color: rgb(238, 226, 226);
-      background-color: rgba(0, 0, 0, 0.534);
-      opacity: 0%;
+      transition: all 1s;
+      border-radius: 10px;
+      img{
+        width: 100%;
+        border-radius: 10px;
 
-      .title{
-        font-weight: bold;
-        font-size: 1rem;
-      }
-
-      i{
-       color: yellow; 
-      }
-
-      button{
-        width: 60%;
       }
     }
 
   }
 
-  .info-row{
-    height: 200px;
-    background-color: black;
-    border-radius: 15px;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+
 
 
 </style>
